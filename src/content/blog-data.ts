23d7190bd18011abf84e0166ffc0c217bca47322
emmanuelkaribiye <emmanuelkaribiye31@gmail.com>
2025-05-07 17:52:43 +0100

    milestones blog

--- src/content/blog-data.ts
+++ src/content/blog-data.ts
@@ -23,6 +23,34 @@ We’ll write code not to finish an assignment or solely to pass an interview, b
 
       `,
     },
+
+    {
+      title: "Milestones",
+      date: "2025-05-07",
+      slug: "Milestones",
+      tags: ["REFLECTION", "PERFORMANCE"],
+      summary: "One chapter ends and another begins.",
+      content: `
+          It’s another quiet evening. The kind where "me" and "myself" sit together again, but this time, the air feels lighter. The kind of evening where the past few years finally fit into a single, deep breath.
+
+Today, the final expo came and went, a swirl of presentations, handshakes, and the electric buzz of possibility. Mechanical Engineering, once just a phrase on an application form, is now a part of who we are. Every late night, every stubborn equation, every design that refused to work until it did.. they led to this.
+
+But in the background, another song is still playing: the rhythm of code, the hum of ideas wanting to be built. That quiet passion is no longer just a side note; it’s a part of the plan.
+
+The future doesn’t look like a straight road. It looks more like open fields, waiting to be explored. Some paths will need to be paved from scratch. Some will need to be torn up and redrawn. And that’s the exciting part.
+
+We’re not standing at the edge of something anymore. We’re stepping into it.
+
+Not from fear.
+Not from pressure.
+From excitement.
+From choice.
+
+Tomorrow isn’t just another day.
+It’s Day One.
+
+      `,
+    },
   
   ];
   
