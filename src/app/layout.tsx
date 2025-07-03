41d7527dd5731ec8ef7aa16283282c48e7cd0d7a
emmanuelkaribiye <emmanuelkaribiye31@gmail.com>
2025-07-03 16:43:38 +0100

    changing meta data title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Emmanuel Karibiye | Blog",
+  title: "Emmanuel Karibiye",
   description: "My personal website and blog",
 };
 
