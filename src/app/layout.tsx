7a438766188eb0e979545199edcbf8dd436444d2
emmanuelkaribiye <emmanuelkaribiye31@gmail.com>
2025-07-07 20:20:54 +0100

    adding analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"; 
 import ClientBody from "./ClientBody";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
         <Providers>
           <ClientBody>{children}</ClientBody>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
